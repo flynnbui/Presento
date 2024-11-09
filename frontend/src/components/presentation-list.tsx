37c26ba8f4f414ed26019b4fec5a3e89c67ad93f
Thant Zin Tun <z5429653@ad.unsw.edu.au>
2024-11-09 13:49:49 +1100

    Fix mobile responsiveness for presentation cards

--- frontend/src/components/presentation-list.tsx
+++ frontend/src/components/presentation-list.tsx
@@ -1,5 +1,6 @@
 import { Context, useContext } from "@/context"
 import { useEffect, useState } from "react"
+//import { useNavigate } from "react-router-dom"
 
 export type PresentationCard = {
   id: string
@@ -9,9 +10,12 @@ export type PresentationCard = {
 }
 
 function dataToCard(data: PresentationCard[], name?: string) {
+  //const navigate = useNavigate()
+  //http://localhost:3000/
+
   let content: JSX.Element[] = data.map(c => {
     return (
-      <div className="aspect-[2/1] rounded-xl bg-zinc-800/50 flex flex-row">
+      <div className="w-full aspect-[2/1] rounded-xl bg-zinc-800/50 flex flex-row">
         <div className="h-[100%] w-[50%] bg-gray-700"></div>
         <div className="h-[100%] w-[50%] flex flex-col justify-start">
           <div className="text-white/80 text-wrap text-1xl ml-4 mt-3">{c.name}</div>
@@ -40,7 +44,7 @@ export function PresentationCards() {
   }, [data])
 
   return (
-    <div className="overflow-auto max-h-[100%] h-full flex flex-col gap-4 justify-between grid auto-rows-min gap-4 md:grid-cols-2  ">
+    <div className="overflow-auto max-h-[100%] h-full grid auto-rows-min gap-4 md:grid-cols-2  ms:grid-cols-1">
       {cards}
     </div>
   )
